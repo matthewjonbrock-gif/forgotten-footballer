21cf1a820d292a8aad0b1781c71d7f162e3dcfdb
matthewjonbrock-gif <matthewjonbrock@gmail.com>
2026-07-23 15:38:47 +0100

    Separate player data and add game setup

---
+++ players.js
@@ -0,0 +1,188 @@
+window.PLAYERS = [
+  {
+    id: "morten-gamst-pedersen",
+    name: "Morten Gamst Pedersen",
+
+    answers: [
+      "morten gamst pedersen",
+      "gamst pedersen",
+      "morten pedersen",
+      "pedersen"
+    ],
+
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/f/fd/Morten_Gamst_Pedersen_vs_England_May_2012_%28cropped%29.jpg",
+
+    source:
+      "https://commons.wikimedia.org/wiki/File:Morten_Gamst_Pedersen_vs_England_May_2012_(cropped).jpg",
+
+    nationality: "Norway",
+    position: "Left midfielder",
+    clubs: ["Blackburn Rovers"],
+    era: "2000s",
+    competition: "Premier League",
+
+    fact:
+      "Remembered for his left foot, dangerous set pieces and spectacular long-range goals.",
+
+    difficulty: 4,
+
+    credit:
+      "John Christian Fjellestad, CC BY-SA 2.0, via Wikimedia Commons",
+
+    focusX: "50%",
+    focusY: "34%"
+  },
+
+  {
+    id: "niko-kranjcar",
+    name: "Niko Kranjčar",
+
+    answers: [
+      "niko kranjcar",
+      "kranjcar",
+      "niko kranjar",
+      "kranjar",
+      "krancjar",
+      "krancjiar"
+    ],
+
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/9/9a/Nikokranjcarfootballer.jpg",
+
+    source:
+      "https://commons.wikimedia.org/wiki/File:Nikokranjcarfootballer.jpg",
+
+    nationality: "Croatia",
+    position: "Attacking midfielder",
+    clubs: [
+      "Portsmouth",
+      "Tottenham Hotspur"
+    ],
+    era: "2000s",
+    competition: "Premier League",
+
+    fact:
+      "A stylish creative midfielder remembered for his technique and ability from distance.",
+
+    difficulty: 5,
+
+    credit:
+      "Daveangelports, public domain, via Wikimedia Commons",
+
+    focusX: "50%",
+    focusY: "38%"
+  },
+
+  {
+    id: "yakubu",
+    name: "Yakubu",
+
+    answers: [
+      "yakubu",
+      "yakubu aiyegbeni",
+      "aiyegbeni",
+      "the yak"
+    ],
+
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/0/0f/Yakubu_in_2013_%28cropped%29.jpg",
+
+    source:
+      "https://commons.wikimedia.org/wiki/File:Yakubu_in_2013_(cropped).jpg",
+
+    nationality: "Nigeria",
+    position: "Striker",
+    clubs: [
+      "Portsmouth",
+      "Middlesbrough",
+      "Everton",
+      "Blackburn Rovers"
+    ],
+    era: "2000s",
+    competition: "Premier League",
+
+    fact:
+      "A powerful and prolific Premier League striker who scored goals for several clubs.",
+
+    difficulty: 3,
+
+    credit:
+      "Alexchen4836, CC BY-SA 4.0, via Wikimedia Commons",
+
+    focusX: "50%",
+    focusY: "20%"
+  },
+
+  {
+    id: "roque-santa-cruz",
+    name: "Roque Santa Cruz",
+
+    answers: [
+      "roque santa cruz",
+      "santa cruz",
+      "roque",
+      "santacruz"
+    ],
+
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/0/04/Roque_Luis_Santa_Cruz.jpg",
+
+    source:
+      "https://commons.wikimedia.org/wiki/File:Roque_Luis_Santa_Cruz.jpg",
+
+    nationality: "Paraguay",
+    position: "Striker",
+    clubs: [
+      "Blackburn Rovers",
+      "Manchester City"
+    ],
+    era: "2000s",
+    competition: "Premier League",
+
+    fact:
+      "His outstanding first Blackburn season made him one of the league's most sought-after strikers.",
+
+    difficulty: 4,
+
+    credit:
+      "Hazaña17, CC BY-SA, via Wikimedia Commons",
+
+    focusX: "50%",
+    focusY: "42%"
+  },
+
+  {
+    id: "tugay",
+    name: "Tugay",
+
+    answers: [
+      "tugay",
+      "tugay kerimoglu",
+      "kerimoglu"
+    ],
+
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/e/ec/Tugay_kerioglu_in_2016.jpg",
+
+    source:
+      "https://commons.wikimedia.org/wiki/File:Tugay_kerioglu_in_2016.jpg",
+
+    nationality: "Turkey",
+    position: "Central midfielder",
+    clubs: ["Blackburn Rovers"],
+    era: "2000s",
+    competition: "Premier League",
+
+    fact:
+      "A Blackburn cult hero remembered for calm midfield control and extraordinary long-range goals.",
+
+    difficulty: 6,
+
+    credit:
+      "Biser Todorov, CC BY-SA 4.0, via Wikimedia Commons",
+
+    focusX: "50%",
+    focusY: "43%"
+  }
+];
